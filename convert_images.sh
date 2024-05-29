f5d7d2536ea4355c3a87cd49d6f575174d4dc0fa
FMuro <fmuro@us.es>
2024-05-29 15:24:19 +0200

    add image conversion to build script

--- convert_images.sh
+++ convert_images.sh
@@ -2,12 +2,15 @@
 
 # run this script in the project's root directory
 
-convert src/assets/img/bg-masthead.jpg  -write mpr:img +delete \
-\( mpr:img -resize 576x +write src/assets/img/bg-masthead_576w.webp \) \
-\( mpr:img -resize 768x +write src/assets/img/bg-masthead_768w.webp \) \
-\( mpr:img -resize 992x +write src/assets/img/bg-masthead_992w.webp \) \
-\( mpr:img -resize 1200x +write src/assets/img/bg-masthead_1200w.webp \) \
-\( mpr:img -resize 1400x +write src/assets/img/bg-masthead_1400w.webp \) \
-null:
+for file in src/assets/img/*.jpg
+do
+  convert $file  -write mpr:img +delete \
+    \( mpr:img -resize 576x +write ${file%.*}_576w.webp \) \
+    \( mpr:img -resize 768x +write ${file%.*}_768w.webp \) \
+    \( mpr:img -resize 992x +write ${file%.*}_992w.webp \) \
+    \( mpr:img -resize 1200x +write ${file%.*}_1200w.webp \) \
+    \( mpr:img -resize 1400x +write ${file%.*}_1400w.webp \) \
+    null:
+done
 
 find src/assets/img/ -name '*.jpg' -execdir mogrify -format webp {} +

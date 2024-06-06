200e53516c00db91357ab3f72f612e23b22cb391
FMuro <fmuro@us.es>
2024-06-06 18:28:30 +0200

    landing icon tootips

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -52,3 +52,7 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+// Activate Bootstrap tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
